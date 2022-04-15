2ac7678c80cfa80a8721b8f113d839e66b039d53
PlasAJulian <julian_aplas@outlook.com>
2022-04-15 16:23:34 -0400

    Added some base files the project will use.

---
+++ src/files/Math-operations.py
@@ -0,0 +1,19 @@
+# Uncomment below to take inputs from the user
+a = float(input('Enter first number: '))
+b = float(input('Enter second number: '))
+
+#Addition  
+c = a + b
+print('Addition of the two number is ' + c) #Prints
+
+#Subtraction
+c = a - b
+print('Subtraction of the two number is ' + c) #Prints
+
+#Multiplication
+c = a * b
+print('Multiplication of the two number is ' + c) #Prints
+
+#Division
+c = a / b
+print('division of the two number is ' + c) #Prints
