b63d1f651ea10bd3ba3750611fbf176c09b9122d
PlasAJulian <julian_aplas@outlook.com>
2022-04-15 16:41:31 -0400

    .

---
+++ out/production/python2java/files/area-triangle.py
@@ -0,0 +1,15 @@
+# Python Program to find the area of triangle
+
+a = 5.6
+b = 6
+
+# Uncomment below to take inputs from the user
+# a = float(input('Enter first side: '))
+# b = float(input('Enter second side: '))
+# c = float(input('Enter third side: '))
+
+
+# calculate the area
+area = (a * b) / 2
+
+print('The area of the triangle is ' + area)
---
+++ out/production/python2java/files/for-loop-sum.py
@@ -0,0 +1,8 @@
+#empty sum
+sum = 0
+
+#for loop that does from 0 to 6
+for x in range(6):
+  sum = sum + x
+  print(sum)
+#for loop that displays the sum 0 to 6
